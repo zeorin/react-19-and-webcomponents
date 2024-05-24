882f162ab64db8f7c3e46a88fb962adbc70def85
Xandor Schiefer <me@xandor.co.za>
2024-05-24 18:59:47 +0200

    formatting

--- src/schalk-example.ts
+++ src/schalk-example.ts
@@ -1,91 +1,91 @@
 const registerCustomElement = (tag: string, ctor: CustomElementConstructor) => {
-	if (!window.customElements.get(tag)) {
-		window.customElements.define(tag, ctor);
-	} else {
-		document.querySelectorAll(tag).forEach((node) => {
-			Object.setPrototypeOf(node, ctor.prototype);
-			(node as Element & { connectedCallback: () => void }).connectedCallback();
-		});
-	}
-};
+  if (!window.customElements.get(tag)) {
+    window.customElements.define(tag, ctor)
+  } else {
+    document.querySelectorAll(tag).forEach((node) => {
+      Object.setPrototypeOf(node, ctor.prototype)
+      ;(node as Element & { connectedCallback: () => void }).connectedCallback()
+    })
+  }
+}
 
-type Colour = NonNullable<React.CSSProperties["color"]>;
+type Colour = NonNullable<React.CSSProperties["color"]>
 
 class SchalkExampleElement extends HTMLElement {
-	inner = this.attachShadow({ mode: "open" });
-	colour?: Colour;
+  inner = this.attachShadow({ mode: "open" })
+  colour?: Colour
 
-	static get observedAttributes() {
-		return ["colour"];
-	}
+  static get observedAttributes() {
+    return ["colour"]
+  }
 
-	constructor() {
-		super();
-	}
+  constructor() {
+    super()
+  }
 
-	attributeChangedCallback(name: string, oldValue: unknown, newValue: unknown) {
-		if (name === "colour" && oldValue !== newValue) {
-			const element = this.inner.querySelector("input");
-			if (!element) return;
-			element.value = newValue as string;
-			this.colour = newValue as string;
-		}
-	}
+  attributeChangedCallback(name: string, oldValue: unknown, newValue: unknown) {
+    if (name === "colour" && oldValue !== newValue) {
+      const element = this.inner.querySelector("input")
+      if (!element) return
+      element.value = newValue as string
+      this.colour = newValue as string
+    }
+  }
 
-	connectedCallback() {
-		const colour = this.getAttribute("colour");
+  connectedCallback() {
+    const colour = this.getAttribute("colour")
 
-		this.inner.innerHTML = /* html */ `
+    this.inner.innerHTML = /* html */ `
       <label>
         <span>Colorize!</span>
         <input value="${colour ?? ""}">
         <button>Apply</button>
       </label>
-    `;
+    `
 
-		this.inner.querySelector("button")?.addEventListener("click", () => {
-			const value = this.inner.querySelector("input")?.value;
+    this.inner.querySelector("button")?.addEventListener("click", () => {
+      const value = this.inner.querySelector("input")?.value
 
-			if (!value) return;
+      if (!value) return
 
-			this.colour = value;
+      this.colour = value
 
-			const payload = new CustomEvent("ColourChange", {
-				detail: value,
-				bubbles: true,
-				composed: true,
-			});
+      const payload = new CustomEvent("ColourChange", {
+        detail: value,
+        bubbles: true,
+        composed: true,
+      })
 
-			this.dispatchEvent(payload);
-		});
-	}
+      this.dispatchEvent(payload)
+    })
+  }
 }
 
-registerCustomElement("schalk-example", SchalkExampleElement);
+registerCustomElement("schalk-example", SchalkExampleElement)
 
-export const SchalkExample = "schalk-example";
+export const SchalkExample = "schalk-example"
 
 interface MyCustomEvent<D = unknown, C = unknown, T = unknown>
-	extends CustomEvent<D> {
-	currentTarget: C & EventTarget;
-	target: T & EventTarget;
+  extends CustomEvent<D> {
+  currentTarget: C & EventTarget
+  target: T & EventTarget
 }
 
 interface ColourChangeEvent<T = Element> extends MyCustomEvent<Colour, T> {}
 
 type ColourChangeEventHandler<T = Element> = {
-	bivarianceHack(event: ColourChangeEvent<T>): void;
-}["bivarianceHack"];
+  bivarianceHack(event: ColourChangeEvent<T>): void
+}["bivarianceHack"]
 
 interface SchalkExampleHTMLAttributes<T> extends React.HTMLAttributes<T> {
-	colour?: Colour | undefined;
-	onColourChange?: ColourChangeEventHandler<T> | undefined;
-	onColourChangeCapture?: ColourChangeEventHandler<T> | undefined;
+  colour?: Colour | undefined
+  onColourChange?: ColourChangeEventHandler<T> | undefined
+  onColourChangeCapture?: ColourChangeEventHandler<T> | undefined
 }
 
 export interface SchalkExampleJSXIntrinsicElement {
-	"schalk-example": React.DetailedHTMLProps<
-		SchalkExampleHTMLAttributes<SchalkExampleElement>,
-		SchalkExampleElement
-	>;
+  "schalk-example": React.DetailedHTMLProps<
+    SchalkExampleHTMLAttributes<SchalkExampleElement>,
+    SchalkExampleElement
+  >
 }
